e0fd74379126350658feba7aaa4785d463e02da0
Jim Northrup <jnorthrup@users.noreply.github.com>
2019-05-06 06:00:35 +0700

    Update SwapBenchmark.java

--- SwapBenchmark.java
+++ SwapBenchmark.java
@@ -7,85 +7,79 @@ public class SwapBenchmark {
 
     public static void main(String[] args) {
         for (var rep = 1; rep < 11; rep++)
-            for (var digits =6; digits < 10; digits += 1) {
+            for (var digits = 6; digits < 10; digits += 1) {
                 var size = (int) Double.parseDouble("1E" + digits);
                 var x = new int[size];
                 for (var i = 0; i < x.length; i++) x[i] = random.nextInt();
                 for (var swapper : swappers.values()) {
-                    var last = 0;
                     final var first = x[0];
                     var begin = System.currentTimeMillis();
-                    last = swapper.Swap(x);
+                    swapper.swap(x);
+                    var last = x[size - 1];
                     if (last != first) throw new Error("swap elevator fail");
                     var l = System.currentTimeMillis() - begin;
-                    System.err.println(swapper+ ": " + size + ":  " + l + " @" + (double) size / (double) l + "/ms");
+                    System.err.println(swapper + ": " + size + ":  " + l + " @" + (double) size / (double) l + "/ms");
 
-                } 
+                }
             }
     }
 
 
     enum swappers {
-        xorSwap() {
-            int Swap(int[] x) {
-                var last = 0;
+        xor() {
+            void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
-                    x[i] = x[i] ^ x[j];
-                    x[j] = x[j] ^ x[i];
-                    x[i] = x[i] ^ x[j];
-                    last = x[j];
+                    x[i] ^= x[j];
+                    x[j] ^= x[i];
+                    x[i] ^= x[j];
                 }
-                return last;
             }
-        }, r64Swap {
+        }, r64 {
             @Override
-            int Swap(int[] x) {
-
-                var last = 0;
+            void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
-                    var x1 = x[i];
-                    var x2 = x[j];
-                    var t = ((long) x1) << 32 | (x2 & 0xffff_ffffL);
+                    final var x1 = x[i];
+                    final var x2 = x[j];
+                    var t = ((long) x1 & 0xffff_ffffL) << 32 | (x2 & 0xffff_ffffL);
                     x[i] = (int) (t & 0xffff_ffffL);
-                    x[j] = (int) (t >> 32);
-                    last = x[j];
+                    x[j] = (int) (t >>> 32);
+                    assert x1 == x[j];
+                    assert x2 == x[i];
                 }
-                return last;
             }
-        }, tmpSwap {
+        }, tmp {
             @Override
-            int Swap(int[] x) {
-                int t;
-                var last = 0;
+            void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
-                    t = x[i];
-                    x[i] = x[j];
+                    final var x1 = x[j];
+                    final var t = x[i];
+                    x[i] = x1;
                     x[j] = t;
-                    last = x[j];
                 }
-                return last;
             }
-        }, xorTmp {
+        }, xtm {
             @Override
-            int Swap(int[] x) {
-                var last = 0;
+            void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
-                    var x1 = x[i];
+//                    int v2, x2 =v2= x[j];
+//                    int v1, x1 =v1= x[i];
                     var x2 = x[j];
-                    x1 = x1 ^ x2;
-                    x2 = x1 ^ x2;
-                    x1 = x1 ^ x2;
+                    var x1 = x[i];
+                    x1 ^= x2;
+                    x2 ^= x1;
+                    x1 ^= x2;
                     x[i] = x1;
                     x[j] = x2;
-                    last = x[j];
+//                    assert v1 == x[j];
+//                    assert v2 == x[i];
                 }
-                return last;
             }
         };
-        abstract int Swap(int[] x);
+
+        abstract void swap(int[] x);
     }
 }
