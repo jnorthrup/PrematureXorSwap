4accd3859c6e0f3bf62d79a43347253c64eef99f
Jim Northrup <jnorthrup@users.noreply.github.com>
2019-05-06 08:40:06 +0700

    Update SwapBenchmark.java

--- SwapBenchmark.java
+++ SwapBenchmark.java
@@ -1,32 +1,49 @@
 package premature.optimization;
 
-import java.util.Random;
+import java.nio.IntBuffer;
+import java.util.*;
+
 
 public class SwapBenchmark {
     static Random random = new Random();
 
     public static void main(String[] args) {
-        for (var rep = 1; rep < 11; rep++)
+        for (var rep = 1; rep < 11; rep++) {
+            Comparator<Map.Entry> entryComparator = (entry, t1) -> (int) ((long) entry.getValue() - (long) t1.getValue());
             for (var digits = 6; digits < 10; digits += 1) {
+                ArrayList<Map.Entry<Swapper, Long>> entries = new ArrayList<>();
                 var size = (int) Double.parseDouble("1E" + digits);
                 var x = new int[size];
                 for (var i = 0; i < x.length; i++) x[i] = random.nextInt();
-                for (var swapper : swappers.values()) {
+                for (var swapper : Swapper.values()) {
                     final var first = x[0];
                     var begin = System.currentTimeMillis();
                     swapper.swap(x);
                     var last = x[size - 1];
                     if (last != first) throw new Error("swap elevator fail");
                     var l = System.currentTimeMillis() - begin;
-                    System.err.println(swapper + ": " + size + ":  " + l + " @" + (double) size / (double) l + "/ms");
+                    entries.add(new AbstractMap.SimpleEntry<>(swapper, l));
 
                 }
+
+                entries.sort(entryComparator);
+                System.err.println("---- for " + size);
+                for (Map.Entry<Swapper, Long> entry : entries) {
+                    Swapper key = entry.getKey();
+                    Long value = entry.getValue();
+                    System.err.println(key + ": " + size + ":  " + value + " @" + (double) size / value + "/ms");
+                }
+                System.err.println();
             }
+        }
     }
 
 
-    enum swappers {
-        xor() {
+    enum Swapper {
+        /**
+         * xor swap
+         */
+        xor_swap() {
             void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
@@ -35,21 +52,41 @@ public class SwapBenchmark {
                     x[i] ^= x[j];
                 }
             }
-        }, r64 {
+        },
+        /**
+         * creates a long, and pushes to it
+         */
+        r64shift {
             @Override
             void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
                     final var j = i + 1;
-                    final var x1 = x[i];
-                    final var x2 = x[j];
-                    var t = ((long) x1 & 0xffff_ffffL) << 32 | (x2 & 0xffff_ffffL);
+                    var t = ((long) x[i] & 0xffff_ffffL) << 32 | (x[j] & 0xffff_ffffL);
                     x[i] = (int) (t & 0xffff_ffffL);
                     x[j] = (int) (t >>> 32);
-                    assert x1 == x[j];
-                    assert x2 == x[i];
+                    assert x[i] == x[j];
+                    assert x[j] == x[i];
                 }
             }
-        }, tmp {
+        },
+        /**
+         * creates a tmp, swaps two array items
+         */
+        tmp1swap {
+            @Override
+            void swap(int[] x) {
+                for (var i = 0; i < x.length - 1; i++) {
+                    final var j = i + 1;
+                    final var t = x[i];
+                    x[i] = x[j];
+                    x[j] = t;
+                }
+            }
+        },
+        /**
+         * creates 2 tmps, swaps two array items
+         */
+        tmp2swap {
             @Override
             void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
@@ -60,7 +97,11 @@ public class SwapBenchmark {
                     x[j] = t;
                 }
             }
-        }, xtm {
+        },
+        /**
+         * we get some kind of optimization from creating two temps and swapping them and writing them, ony my cpu slightly tweaked by the read and write order from the array.
+         */
+        xor_tmps {
             @Override
             void swap(int[] x) {
                 for (var i = 0; i < x.length - 1; i++) {
@@ -77,6 +118,85 @@ public class SwapBenchmark {
 //                    assert v1 == x[j];
 //                    assert v2 == x[i];
                 }
+            }
+        },
+        /**
+         * heapbytebuffer random access get and put.  roughly the same as tmp swap
+         */
+        hbb_racc {
+            void swap(int[] x) {
+                var wrap = IntBuffer.wrap(x);
+                for (var i = 0; i < x.length - 1; i++) {
+                    final var j = i + 1;
+                    var xi = wrap.get(i);
+                    wrap.put(i, wrap.get(j));
+                    wrap.put(j, xi);
+                }
+            }
+        },
+        /**
+         * heapbytebuffer using mark and rewind naively
+         */
+        hbb_mark {
+            void swap(int[] x) {
+//                int l1 = x[0];
+//                int li = x[x.length - 2];
+//                int lj = x[x.length - 1];
+                var wrap = IntBuffer.wrap(x).mark();
+                while (wrap.remaining() > 1) {
+                    var i = wrap.mark().get();
+                    var j = wrap.get();
+                    wrap.reset();
+                    wrap.put(j);
+                    wrap.mark().put(i).reset();
+                }
+            }
+        },
+        /**
+         * heap bytebuffer calling position instead of mark
+         */
+        hbb_cpos {
+            void swap(int[] x) {
+//                int l1 = x[0];
+//                int li = x[x.length - 2];
+//                int lj = x[x.length - 1];
+                var wrap = IntBuffer.wrap(x);
+                while (wrap.remaining() > 1) {
+                    var mark = wrap.position();
+                    var i = wrap.get();
+                    var j = wrap.get();
+                    wrap.position(mark).put(j);
+                    wrap.put(i).position(mark + 1);
+                }
+            }
+        },
+        /**
+         * 4 forward moving get/put heapbytebuffers particular to this benchmark
+         */
+        hbb_4way {
+            void swap(int[] x) {
+
+//                int l1 = x[0];
+//                int li = x[x.length - 2];
+//                int lj = x[x.length - 1];
+                var write2 = IntBuffer.wrap(x);
+                var write1 = write2.slice();
+
+                var lead = write2.slice();
+                var trail = write2.slice();
+
+                lead.position(1);
+                write2.position(1);
+                var i = 0;
+                var j = 0;
+                while (write2.remaining() > 0) {
+                    i = lead.get();
+                    j = trail.get();
+                    write1.put(i);
+                    write2.put(j);
+                }
+//                int x1 = x[x.length - 1];
+//                System.err.println(Arrays.asList(i,j, x1));
             }
         };
 
